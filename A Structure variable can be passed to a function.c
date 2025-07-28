99f433ae2b54e65a097121fe125779a63bf3e8bd
nitin-1-9 <nk53015scam@gmail.com>
2025-07-28 10:55:01 +0530

    Update A Structure variable can be passed to a function.c

--- A Structure variable can be passed to a function.c
+++ A Structure variable can be passed to a function.c
@@ -6,7 +6,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-#include <stdio.h>
+#include <stdio.h>    //NITIN
 #include<string.h>
  typedef struct pokemon{
         char name[10];
